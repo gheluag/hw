e236943caa426230e705f8a7e290e5229d6e06d8
gheluag <jaba@gmail.com>
2022-11-03 10:30:52 +0300

    defbuil

---
+++ bufunc/t7.py
@@ -0,0 +1,17 @@
+# не удалось понять декораторы ;_;
+# попытка что-то сделать
+
+from os import name
+def dec(funcdec):
+  def wrapper():
+    print("Hello,", name)
+  funcdec()
+  return wrapper
+
+@dec
+def get_name():
+  name = input('Введите имя: ')
+  # print(name)
+  return name
+  
+get_name()
